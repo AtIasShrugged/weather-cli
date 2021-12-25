c8bce34c07932dc82a3dfa4c5abd13a6f6c9aec7
AtIasShrugged <btw210899@gmail.com>
2021-12-25 22:10:13 +0200

    Added initial storage-service

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
 import { printHelp } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
 
 const main = () => {
   const args = getArgs(process.argv);
   if (args.h) {
     printHelp();
   }
-  if (args.s) {
-    // save city
+  if (args.c) {
+    saveKeyValue("city", args.s);
   }
   if (args.t) {
-    // save token
+    saveKeyValue("token", args.t);
   }
   // print weather
 };
