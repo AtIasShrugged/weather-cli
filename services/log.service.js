9af8a7e66890432d277d070eca8e9382c2f26edf
Atlas <btw210899@gmail.com>
2021-12-25 21:53:46 +0200

    Added log-service for printing command outputs

---
+++ services/log.service.js
@@ -0,0 +1,23 @@
+import chalk from "chalk";
+import dedent from "dedent-js";
+
+const printError = (err) => {
+  console.log(chalk.bgRed(" Error  ") + err);
+};
+
+const printSuccess = (msg) => {
+  console.log(chalk.bgGreen(" Success  ") + msg);
+};
+
+const printHelp = () => {
+  console.log(
+    dedent`${chalk.bgCyan(" HELP ")}
+	 ${chalk.green.bold("w/o params")} to print weather for the selected city
+	 ${chalk.yellow.bold("-s [CITY]")} to set up the city
+	 ${chalk.magenta.bold("-t [API_KEY]")} to set up the token
+	 ${chalk.cyan.bold("-h")} to output the help
+`
+  );
+};
+
+export { printError, printSuccess, printHelp };
