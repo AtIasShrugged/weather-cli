2986845e0d69a290d30db1041d9d5882e21af493
AtIasShrugged <btw210899@gmail.com>
2021-12-25 23:00:50 +0200

    Added storage-service logic
    Added logic in storage-service for get/set data from/to file in user corner directory
    Added logic for checking is directory exist

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,8 +1,39 @@
 import { homedir } from "os";
 import { join } from "path";
+import { promises } from "fs";
 
 const filePath = join(homedir(), "weather-data.json");
 
-const saveKeyValue = (key, value) => {};
+const saveKeyValue = async (key, value) => {
+  let data = await getData();
+  data[key] = value;
+  await promises.writeFile(filePath, JSON.stringify(data));
+};
 
-export { saveKeyValue };
+const getKeyValue = async () => {
+  const data = await getData();
+  if (data) {
+    return data[key];
+  }
+  return undefined;
+};
+
+const getData = async () => {
+  let data = {};
+  if (await isExist(filePath)) {
+    const file = await promises.readFile(filePath);
+    data = JSON.parse(file);
+  }
+  return data;
+};
+
+const isExist = async (path) => {
+  try {
+    await promises.stat(path);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+export { saveKeyValue, getKeyValue };
