74a9d1f4644a21e5717d06cfdb712db2a4fd3741
AtIasShrugged <btw210899@gmail.com>
2021-12-26 15:18:05 +0200

    Updated data-handlers
    Added save-city hander
    Added get-forcast handler

--- helpers/data-handlers.js
+++ helpers/data-handlers.js
@@ -1,18 +1,59 @@
+import { getWeather } from "../services/api.service.js";
 import { printError, printSuccess } from "../services/log.service.js";
-import { saveKeyValue } from "../services/storage.service.js";
+import { getKeyValue, saveKeyValue } from "../services/storage.service.js";
 
 const saveToken = async (token) => {
   try {
     if (!token.length) {
-      throw new Error("Token not passed");
+      throw new Error("Некоректный токен");
     }
     await saveKeyValue("token", token);
-    printSuccess("Token saved successfully");
+    printSuccess("Токен сохранён");
   } catch (e) {
     printError(e.message);
   }
 };
 
-const saveCity = async (city) => {};
+const saveCity = async (city) => {
+  try {
+    if (!city.length) {
+      throw new Error("Некоректный город");
+    }
 
-export { saveToken, saveCity };
+    await saveKeyValue("city", city);
+    printSuccess("Город сохранён");
+  } catch (e) {
+    printError(e.message);
+  }
+};
+
+const getForcast = async () => {
+  try {
+    const city = await getKeyValue("city");
+    if (!city) {
+      throw new Error(
+        "Не установлен город.\nУстановите его командой -c [CITY]"
+      );
+    }
+    const weather = await getWeather(city);
+    printSuccess(`
+    ${weather.name}: ${weather.weather[0].description}
+    ${Math.round(weather.main.temp)}°C - чувствуется как ${Math.round(
+      weather.main.feels_like
+    )}°C
+    `);
+  } catch (e) {
+    if (e?.response?.status == 404) {
+      printError("Некорректный город");
+      return;
+    }
+    if (e?.response?.status == 401) {
+      printError("Некорректный токен");
+      return;
+    } else {
+      printError(e.message);
+    }
+  }
+};
+
+export { saveToken, saveCity, getForcast };
--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
-import { saveToken } from "./helpers/data-handlers.js";
+import { getForcast, saveCity, saveToken } from "./helpers/data-handlers.js";
 import { printHelp } from "./services/log.service.js";
-import { saveKeyValue } from "./services/storage.service.js";
 
 const main = () => {
   const args = getArgs(process.argv);
   if (args.h) {
     printHelp();
+    return;
   }
   if (args.c) {
-    // return saveCity(args.c);
+    return saveCity(args.c);
   }
   if (args.t) {
     return saveToken(args.t);
   }
+  getForcast();
 };
 
 main();
